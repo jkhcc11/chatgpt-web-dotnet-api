b9531aae3e335016a39e05b37b33dfc248edbc58
成 <137651076@qq.com>
2023-06-18 17:10:14 +0800

    冲突处理

--- src/ChatGpt.Web.MongoDB/Repository/PerUseActivationCodeRecordRepository.cs
+++ src/ChatGpt.Web.MongoDB/Repository/PerUseActivationCodeRecordRepository.cs
@@ -10,7 +10,8 @@ namespace ChatGpt.Web.MongoDB.Repository
     /// <summary>
     /// 按次卡密记录 仓储实现
     /// </summary>
-    public class PerUseActivationCodeRecordRepository : BaseMongodbRepository<PerUseActivationCodeRecord, long>, IPerUseActivationCodeRecordRepository
+    public class PerUseActivationCodeRecordRepository : BaseMongodbRepository<PerUseActivationCodeRecord, long>,
+        IPerUseActivationCodeRecordRepository
     {
         public PerUseActivationCodeRecordRepository(GptWebMongodbContext gptWebMongodbContext) : base(gptWebMongodbContext)
         {
@@ -23,39 +24,21 @@ namespace ChatGpt.Web.MongoDB.Repository
         /// <param name="cardNo">卡号</param>
         /// <param name="modelGroupName">模型分组名</param>
         /// <returns></returns>
-        public async Task<int> CountTimesByGroupNameAsync(DateTime date, string cardNo
-            , string modelGroupName)
+        public async Task<int> CountTimesByGroupNameAsync(string cardNo
+            , string modelGroupName, DateTime? date)
         {
-            var startTime = date.Date;
-            var endTime = Convert.ToDateTime($"{date:yyyy-MM-dd 23:59:59}");
-
             var query = DbCollection.AsQueryable()
-                .Where(a => a.CreatedTime >= startTime &&
-                            a.CreatedTime <= endTime &&
-                            a.CardNo == cardNo &&
+                .Where(a => a.CardNo == cardNo &&
                             a.ModelGroupName == modelGroupName);
+            if (date.HasValue)
+            {
+                var startTime = date.Value.Date;
+                var endTime = Convert.ToDateTime($"{date:yyyy-MM-dd 23:59:59}");
+                query = query.Where(a => a.CreatedTime >= startTime &&
+                                         a.CreatedTime <= endTime);
+            }
+
             return await query.CountAsync();
         }
-
-        /// <summary>
-        /// 根据日期模型Id统计次数
-        /// </summary>
-        /// <param name="date">日期</param>
-        /// <param name="cardNo">卡号</param>
-        /// <param name="modelId">模型ID</param>
-        /// <returns></returns>
-        public async Task<int> CountTimesByModelIdAsync(DateTime date, string cardNo, string modelId)
-        {
-            var startTime = date.Date;
-            var endTime = Convert.ToDateTime($"{date:yyyy-MM-dd 23:59:59}");
-
-            var query = DbCollection.AsQueryable()
-                .Where(a => a.CreatedTime >= startTime &&
-                            a.CreatedTime <= endTime &&
-                            a.CardNo == cardNo &&
-                            a.ModelId == modelId);
-            return await query.CountAsync();
-        }
-
     }
 }
